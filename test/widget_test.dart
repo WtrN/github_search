b60b493bfaa9a6595680aeae375b496962473994
WtrN <nishi.w.136@gmail.com>
2022-04-23 16:49:59 +0900

    Flavorによるだし分け設定を追加

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:githubsearch/main.dart';
+import 'package:githubsearch/main_ded.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
